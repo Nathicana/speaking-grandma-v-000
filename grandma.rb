f225bc4d82e01d40601bc101176737564c3271bd
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-10 01:49:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,12 @@
 
 
 def speak_to_grandma(phrase)
-    
+    if phrase == 
     
     elseif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  else 
-    
+   else 
+    puts "HUH?! SPEAK UP, SONNY!"
   
 end
 end
